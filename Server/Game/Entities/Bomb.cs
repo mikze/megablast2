857f712c31972fd9c6d5bacadc07daa0e55510a8
Michal Zembik <mikzepro@gmail.com>
2024-10-28 16:28:52 +0100

    Refactor 2

--- Server/Game/Entities/Bomb.cs
+++ Server/Game/Entities/Bomb.cs
@@ -18,13 +18,13 @@ public class Bomb : EntityBase
         Height = Width = 36;
         Collision = true;
 
-        new Task(async () =>
+        new Task( () =>
         {
             try
             {
                 Console.WriteLine($"PLANT BOMB");
-                await Task.Delay(2000);
-                await Explode();
+                Thread.Sleep(2000);
+                Explode();
             }
             catch(Exception e)
             {
@@ -34,13 +34,13 @@ public class Bomb : EntityBase
 
     }
 
-    private async Task SendToClients(List<Fire> fires)
+    private void SendToClients(List<Fire> fires)
     {
-        await Game.HubGameService?.HubContext.Clients.All.SendAsync("Fires", fires.ToArray())!;
-        await Game.HubGameService.HubContext.Clients.All.SendAsync("BombExplode", new BombModel(this));
+         Game.HubGameService?.HubContext.Clients.All.SendAsync("Fires", fires.ToArray());
+         Game.HubGameService?.HubContext.Clients.All.SendAsync("BombExplode", new BombModel(this));
     }
 
-    private static async Task<List<Fire>> Calc(List<Fire> fires)
+    private static List<Fire> Calc(List<Fire> fires)
     {
         var fireList = new List<Fire>();
 
@@ -51,31 +51,33 @@ public class Bomb : EntityBase
             foreach (var e in coll)
                 if (e is { Destroyed: false } && e.CheckCollision(f))
                 {
-                    if (e is Bomb bomb)
+                    switch (e)
                     {
-                        Console.WriteLine($"Destroy BOMB: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
-                        await bomb.Explode();
-                    }
-                    if (e is Wall)
-                    {
-                        Console.WriteLine($"Destroy block: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
-                        e.Destroyed = true;
-                        Game.CreateBonus(e);
-                    }
-                    if (e is Bonus)
-                    {
-                        Console.WriteLine($"Destroy bonus: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
-                        e.Destroyed = true;
-                    }
-                    if (e is Player player)
-                    {
-                        Console.WriteLine($"Hit player {e.Id}  {player.Name} with {player.Lives} lives");
-                        player.Lives--;
-                        if (player.Lives <= 0)
+                        case Bomb bomb:
+                            Console.WriteLine($"Destroy BOMB: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
+                            bomb.Explode();
+                            break;
+                        case Wall:
+                            Console.WriteLine($"Destroy block: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
+                            e.Destroyed = true;
+                            Game.CreateBonus(e);
+                            break;
+                        case Bonus:
+                            Console.WriteLine($"Destroy bonus: {e.PosX} {e.PosY};  fire: {f.PosX}  {f.PosY}");
+                            e.Destroyed = true;
+                            break;
+                        case Player player:
                         {
-                            Console.WriteLine($"Killed player {e.Id}  {player.Name}");
-                            player.Dead = true;
-                            await Game.HubGameService?.HubContext.Clients.All.SendAsync("KillPlayer", e.Id)!;
+                            Console.WriteLine($"Hit player {e.Id}  {player.Name} with {player.Lives} lives");
+                            player.Lives--;
+                            if (player.Lives <= 0)
+                            {
+                                Console.WriteLine($"Killed player {e.Id}  {player.Name}");
+                                player.Dead = true;
+                                Game.HubGameService?.HubContext.Clients.All.SendAsync("KillPlayer", e.Id);
+                            }
+
+                            break;
                         }
                     }
 
@@ -91,7 +93,7 @@ public class Bomb : EntityBase
         return fireList;
     }
 
-    private async Task Explode()
+    private void Explode()
     {
         if (!Destroyed)
         {
@@ -119,11 +121,11 @@ public class Bomb : EntityBase
                     fires4.Add(new Fire() { PosX = PosX + (i * 32), PosY = PosY });
             }
 
-            var f = await Calc(fires1);
-            f.AddRange(await Calc(fires2));
-            f.AddRange(await Calc(fires3));
-            f.AddRange(await Calc(fires4));
-            await SendToClients(f);
+            var f =  Calc(fires1);
+            f.AddRange( Calc(fires2));
+            f.AddRange( Calc(fires3));
+            f.AddRange( Calc(fires4));
+            SendToClients(f);
         }
     }
 }
